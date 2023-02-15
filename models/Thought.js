26729b77a27b5d309cea9cae942eb54ed6050f3d
Andrew <andrewg87@gmail.com>
2023-02-14 19:28:54 -0500

    filled out the thought.js model a bit, still need to add

---
+++ models/Thought.js
@@ -0,0 +1,19 @@
+const { Schema, model } = require('mongoose');
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280,
+    },
+    createdAt: {
+
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+  }
+)
