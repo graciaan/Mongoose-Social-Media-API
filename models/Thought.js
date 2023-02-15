7b4fdb58759dd4e3892d8298f31646e483a45763
Andrew <andrewg87@gmail.com>
2023-02-14 20:04:38 -0500

    added to thought model, add boilerplate code to index.js, installed package.json files

--- models/Thought.js
+++ models/Thought.js
@@ -1,4 +1,34 @@
 const { Schema, model } = require('mongoose');
+const moment = require('moment');
+
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a'),
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+)
 
 const thoughtSchema = new Schema(
   {
@@ -9,11 +39,27 @@ const thoughtSchema = new Schema(
       maxlength: 280,
     },
     createdAt: {
-
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a'),
     },
     username: {
       type: String,
       required: true,
     },
+    reactions: [reactionSchema],    
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
   }
 )
+
+//Create a virtual called `reactionCount` that retrieves the length of the thought's `reactions` array field on query.
+
+const Thought = model("Thought", thoughtSchema);
+
+module.exports = Thought;
