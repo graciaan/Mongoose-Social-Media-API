fae98b2cc07ef3d6437a8d085b629a803fa99896
Andrew <andrewg87@gmail.com>
2023-02-14 19:10:30 -0500

    added a few files and coded the user model, still need to add to user model

---
+++ models/User.js
@@ -0,0 +1,42 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      //find out how to match a valid email address (look into Mongoose's matching validation)
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJson: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+//find out about creating a virtual `friendCount` that retrieves the length of the user's `friends` array field on query.
+
+const User = model("User", UserSchema);
+
+module.exports = User;
