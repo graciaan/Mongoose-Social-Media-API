bfa3d2601ab1f223668c5191788a14f197a74566
Andrew <andrewg87@gmail.com>
2023-02-17 18:50:13 -0500

    set up route files, added code to index.js in api folder

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -66,35 +66,35 @@ module.exports = {
         : res.json({ message: 'Thought successfully deleted' })
     )
     .catch((err) => res.status(500).json(err));
-},
-//creates reaction
-createReaction(req, res) {
-  Thought.findOneAndUpdate(
-    { _id: req.params.thoughtId },
-    { $addToSet: { reactions: req.body }},
-    { runValidators: true,
-      new: true}
-  )
-  .then((thought) =>
-  !thought
-  ? res.status(404).json({ message: 'No thought found with that ID'})
-  : res.json(thought)
-  )
-  .catch((err) => res.status(500).json(err));
-},
-//delete reaction
-deleteReaction(req, res) {
-  Thought.findOneAndUpdate(
-    { _id: req.params.thoughtId },
-    { $pull: { reactions: {reactionId: req.params.reactionId}}},
-    { runValidators: true,
-      new: true },
-  )
-  .then((thought) => 
-  !thought
-  ? res.status(404).json({ message: 'No thought found with that ID'})
-  : res.json(thought)
-  )
-  .catch((err) => res.status(500).json(err));
-},
-}
+  },
+  //creates reaction
+  createReaction(req, res) {
+    Thought.findOneAndUpdate(
+      { _id: req.params.thoughtId },
+      { $addToSet: { reactions: req.body }},
+      { runValidators: true,
+        new: true}
+    )
+    .then((thought) =>
+    !thought
+    ? res.status(404).json({ message: 'No thought found with that ID'})
+    : res.json(thought)
+    )
+    .catch((err) => res.status(500).json(err));
+  },
+  //deletes reaction
+  deleteReaction(req, res) {
+    Thought.findOneAndUpdate(
+      { _id: req.params.thoughtId },
+      { $pull: { reactions: {reactionId: req.params.reactionId}}},
+      { runValidators: true,
+        new: true },
+    )
+    .then((thought) => 
+    !thought
+    ? res.status(404).json({ message: 'No thought found with that ID'})
+    : res.json(thought)
+    )
+    .catch((err) => res.status(500).json(err));
+  },
+};
