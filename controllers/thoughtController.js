bdbd416fd531fc863083e628c86fa6031403ee2e
Andrew <andrewg87@gmail.com>
2023-02-17 10:05:10 -0500

    more code added to thought controller

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -13,10 +13,39 @@ module.exports = {
       .select('-__v')
       .then((thought) =>
         !thought
-          ? res.status(404).json({ message: 'No user with that ID' })
+          ? res.status(404).json({ message: 'No thought with that ID' })
           : res.json(thought)
       )
       .catch((err) => res.status(500).json(err));
   },
-  
+  //creates new thought and pushes to user's thought array
+  createThought(req, res) {
+    Thought.create(req.body)
+    .then(({ _id }) => {
+      return User.findOneAndUpdate(
+        { _id: req.body.userId },
+        { $push: { thoughts: _id }},
+        { new: true }
+      )
+    })
+    .then((user) =>
+    !user
+    ? res.status(404).json({ message: 'No user with that ID' })
+    : res.json(user)
+    )
+    .catch((err) => res.status(500).json(err));
+  },
+  //updates thought
+  updateThought({ params, body }, res) {
+    Thought.findOneAndUpdate({ _id: params.id }, body, {
+      new: true,
+      runValidators: true,
+    })
+    .then((user) =>
+    !user
+    ? res.status(404).json({ message: 'No thought with that ID'})
+    : res.json(thought)
+    )
+    .catch((err) => res.status(500).json(err));
+  }
 }
