a7f83dbc18b48e8c41d171e0e19bb920a565f115
Andrew <andrewg87@gmail.com>
2023-02-17 19:27:40 -0500

    updated userRoutes

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,22 @@
+const router = require('express').Router();
+
+const {
+  getUsers,
+  getSingleUser,
+  createUser,
+  deleteUser,
+  updateUser,
+  addFriend,
+  deleteFriend
+} = require('../../controllers/userController');
+
+// /api/users
+router.route('/').get(getUsers).post(createUser);
+
+// /api/users/:id
+router.route("/:id").get(getSingleUser).put(updateUser).delete(deleteUser);
+
+// /api/users/:userId/friends/:friendId
+router.route("/:userId/friends/:friendId").post(addFriend).delete(deleteFriend);
+
+module.exports = router;
